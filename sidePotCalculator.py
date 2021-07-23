c49bf508a6cf0a8b23297d9891ef30ca1ee56d41
Donovan Ho <donovanduyho@gmail.com>
2021-07-23 19:16:00 -0400

    Update sidePotCalculator.py

--- sidePotCalculator.py
+++ sidePotCalculator.py
@@ -6,3 +6,43 @@
 #Remaining amount will be pushed into side pot
 #Inform the user of which individuals can take from the side pot and main pot in the case he/she wins
 
+print('')
+print('Welcome to the Side Pot Calculator!')
+
+userChoice = input('CALCULATE SIDE POT? (Y/N): ')
+userChoice = userChoice.upper()
+print('')
+
+if (userChoice == 'Y'):
+    mainPot = int(input('CURRENT VALUE OF POT: '))
+    lowestBet = int(input('LOWEST BET AMOUNT: '))
+    highestBet = int(input('HIGHEST BET AMOUNT: '))
+    playersInHand = int(input('TOTAL PLAYERS IN THE HAND: '))
+
+    mainPotAdd = (lowestBet * playersInHand) + mainPot
+    sidePot = ((highestBet - lowestBet) * (playersInHand - 1)) 
+    
+    print('')
+    print('MAIN POT:', mainPotAdd)
+    print('SIDE POT:', sidePot)
+    print('')
+
+    print('DISTRIBUTION GUIDE')
+    print('==================')
+    print('IF ALL-IN PLAYER WINS: ')
+    print('     ALL-IN PLAYER = MAIN POT')
+    print('     RUNNER-UP PLAYER = SIDE POT')
+    print('')
+    print('IF ALL-IN PLAYER LOSES: ')
+    print('     WINNING PLAYER = MAIN POT + SIDE POT')
+    print('')
+else:
+    print('Thank You.')
+    print('')
+
+
+
+
+
+
+
