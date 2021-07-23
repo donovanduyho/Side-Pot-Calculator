52e0c8fed83d1c19c47611237c5cea92bea0ac7c
Donovan Ho <donovanduyho@gmail.com>
2021-07-23 17:54:23 -0400

    Create sidePotCalculator.py

---
+++ sidePotCalculator.py
@@ -0,0 +1,8 @@
+#Side Pot Calculator 
+#Take input from user to get value for the main pot 
+#Take input from user to get value of lowest bet size player
+#Take input from user to get values of all other players remaining in the hand
+#Perform calculations to get the amount that needs to be pushed into the main pot 
+#Remaining amount will be pushed into side pot
+#Inform the user of which individuals can take from the side pot and main pot in the case he/she wins
+
